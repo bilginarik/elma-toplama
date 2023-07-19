7d52d307842adb5b56c9bd9a9a4c5225de91c00a
bilginarik <79409029+bilginarik@users.noreply.github.com>
2023-07-19 18:37:16 +0300

    Oyun Durdurma, Can Barı, Oyun Kaybetme Menüsü
    Can barı, Oyun Kaybetme Menüsü, oyunu durdurup tekrar başlatma gibi özellikler oyuna eklendi.

--- elma-toplama/Assets/Scripts/Floor.cs
+++ elma-toplama/Assets/Scripts/Floor.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Floor : MonoBehaviour
 {
+    float health = 100.0f;
+    float current_health = 100.0f;
+
+    public GameObject pausePanel;
+    public Image healthbar;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "apple")
         {
             Destroy(collision.gameObject);
+
+            current_health -= 10.0f;
+            healthbar.fillAmount = current_health / health;
+
+            if (current_health <=0)
+            {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+
         }
     }
 }
--- elma-toplama/Assets/Scripts/game_crtl.cs
+++ elma-toplama/Assets/Scripts/game_crtl.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class game_crtl : MonoBehaviour
 {
     public GameObject Apple;
+    bool pauseGame = false;
 
+    public void new_game()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+        Time.timeScale = 1.0f;
+    }
+    public void pause_game()
+    {
+        pauseGame =!pauseGame;
+        if (pauseGame == true)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
     private void Start()
     {
         InvokeRepeating("add_appl", 0.0f, 1.0f);
